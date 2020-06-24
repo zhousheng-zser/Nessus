14b7acb3ad3d2c2bcb11ca50b7988838edb7f6f9
lipb <pingbao.li@glasssix.com>
2020-06-24 11:18:27 +0800

    error以上级别的日志和error以下级别的日志进行分离，方便后续排查错误

--- parser/java/dialoger/src/main/java/com/glasssix/server/protocol/ProtocolCache.java
+++ parser/java/dialoger/src/main/java/com/glasssix/server/protocol/ProtocolCache.java
@@ -1,5 +1,6 @@
 package com.glasssix.server.protocol;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 
+@Slf4j
 @Component
 public class ProtocolCache {
     private final static Map<String,String> startedMap = new HashMap<>();
@@ -15,12 +17,14 @@ public class ProtocolCache {
 
     @Autowired
     public ProtocolCache(ProtocolConfig protocolConfig) {
+        log.info("start load protocol register info!");
         List<ProtocolRegisterEntry> registers = protocolConfig.getRegisters();
         for(ProtocolRegisterEntry r: registers){
             if(r.isStart()){
                 startedMap.put(r.getName(),r.getEntry());
             }
         }
+        log.info("loaded register info all({}) and started({})!",registers.size(),startedMap.entrySet().size());
     }
 
     public static String getEntryName(String algorithmName){
