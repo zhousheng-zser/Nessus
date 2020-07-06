4e0dc82f3fa766fff4f382c3684a9eed97ba241e
lipb <pingbao.li@glasssix.com>
2020-07-06 10:33:54 +0800

    代码整理和将rabbit配置成编译机上的地址

--- parser/java/dialoger/src/main/java/com/glasssix/server/protocol/irisviel/PersonDBDeleteProtocol.java
+++ parser/java/dialoger/src/main/java/com/glasssix/server/protocol/irisviel/PersonDBDeleteProtocol.java
@@ -1,11 +1,6 @@
 package com.glasssix.server.protocol.irisviel;
 
-import com.glasssix.server.algorithm.AlgorithmFactory;
-import com.glasssix.server.protocol.ProtocolCommon;
-import com.glasssix.server.util.ApplicationConstants;
-import com.google.gson.annotations.SerializedName;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StringUtils;
 
 @Slf4j
 public class PersonDBDeleteProtocol extends PersonDBProtocolCommon {
