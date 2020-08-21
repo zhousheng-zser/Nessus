00e460e91e31a28a3eb2fe0951fa6f25e0a1f50a
mingyu92 <mingyu92@126.com>
2020-08-21 16:33:19 +0800

    adapted vision_service to the latest version.

--- register/cpp/VisionService/vision_service.hpp
+++ register/cpp/VisionService/vision_service.hpp
@@ -2,23 +2,26 @@
 
 #include "PluginRegister/plugin_interface.hpp"
 
+#include <mutex>
 #include <functional>
 #include <unordered_map>
 
 #include <abi/consumer.hpp>
-#include <abi/implements.hpp>
 
 namespace glasssix::exposing::nessus
 {
 	class vision_service : public implements<vision_service, plugin_interface>, public make_plugin_qualified_name
 	{
 	public:
+		class impl;
+
 		vision_service();
+		~vision_service();
 		param_string name() const;
 		param_string version() const;
 		param_vector<param_string> get_available_functions() const;
 		unknown_object execute(const param_string& function_name, const param_hash_map<param_string, unknown_object>& params) const;
 	private:
-		std::unordered_map<param_string, std::function<unknown_object(const param_hash_map<param_string, unknown_object>&)>> functions_;
+		impl* impl_;
 	};
 }
