627d0777c4ca9a938c63de5201f90bb3f20a40af
mingyu92 <mingyu92@126.com>
2020-10-21 16:23:47 +0800

    Added comments for license and parser

--- license/cpp/client/license.hpp
+++ license/cpp/client/license.hpp
@@ -18,7 +18,31 @@ using request_license_async_callback_type = void(*)(void* context, bool success,
 using license_deadline_callback_type = void(*)(void* context, const char* message, std::int64_t remaining_seconds);
 using evaluate_license_callback_type = void(*)(void* context, bool success, const char* message, std::int64_t remaining_seconds);
 
+/// <summary>
+/// Initializes the license system.
+/// </summary>
+/// <param name="license_key">The license key provided by the service provider</param>
 extern "C" EXPORT_NESSUS_LICENSE void init_license_system(const char* license_key);
+
+/// <summary>
+/// Evaluates whether a license is valid.
+/// </summary>
+/// <param name="callback">The callback with a result</param>
+/// <param name="context">The user-defined context</param>
 extern "C" EXPORT_NESSUS_LICENSE void evaluate_license(evaluate_license_callback_type callback, void* context = nullptr);
+
+/// <summary>
+/// Requests the license server asynchronously for a new license.
+/// </summary>
+/// <param name="callback">The callback with a result</param>
+/// <param name="context">The user-defined context</param>
 extern "C" EXPORT_NESSUS_LICENSE void request_license_async(request_license_async_callback_type callback, void* context = nullptr);
+
+/// <summary>
+/// Sets a license deadline callback.
+/// </summary>
+/// <param name="callback"></param>
+/// <param name="context"></param>
+/// <returns></returns>
+/// <remarks>The deadline callback triggers every five minutes when the remaining time is less than five days</remarks>
 extern "C" EXPORT_NESSUS_LICENSE void set_license_deadline_callback(license_deadline_callback_type callback, void* context = nullptr);
--- parser/cpp/parser_c.hpp
+++ parser/cpp/parser_c.hpp
@@ -16,10 +16,48 @@
 extern "C" {
 #endif
 
+	/// <summary>
+	/// Creates a new instance of a protocol praser.
+	/// </summary>
+	/// <returns>The instance</returns>
+	/// <remarks>The instance must be freed by parser_release_instance function</remarks>
 	PARSER_C_EXPORT void* parser_new_instance();
+
+	/// <summary>
+	/// Disposes a instance of a protocol parser.
+	/// </summary>
+	/// <param name="instance">The instance</param>
 	PARSER_C_EXPORT void parser_release_instance(void* instance);
+
+	/// <summary>
+	/// Initializes all plugins.
+	/// </summary>
+	/// <param name="instance">The instance of a protocol parser</param>
+	/// <param name="config_file_path">The config file path</param>
+	/// <param name="len">The size of the config file path</param>
+	/// <param name="license_key">The license key provided by the service provider</param>
+	/// <returns>The result interpreted as JSON</returns>
+	/// <remarks>The return value must be disposed by calling praser_free function.</remarks>
 	PARSER_C_EXPORT char* parser_init_plugin(void* instance, char* config_file_path, int len, char* license_key);
+
+	/// <summary>
+	/// Dispatches a protocol.
+	/// </summary>
+	/// <param name="instance">The instance of a protocol parser</param>
+	/// <param name="protocol">The protocol name</param>
+	/// <param name="protocol_len">The size of the protocol name</param>
+	/// <param name="jstr">The parameters interpreted as JSON</param>
+	/// <param name="jstr_len">The size of the parameters</param>
+	/// <param name="data">The optional binary data</param>
+	/// <param name="data_len">The size of the optional binary data</param>
+	/// <returns>The result interpreted as JSON</returns>
+	/// <remarks>The return value must be disposed by calling praser_free function.</remarks>
 	PARSER_C_EXPORT char* parser_parse(void* instance, char* protocol, int protocol_len, char* jstr, int jstr_len, char* data, int data_len);
+
+	/// <summary>
+	/// Disposes a buffer.
+	/// </summary>
+	/// <param name="ptr">The buffer</param>
 	PARSER_C_EXPORT void parser_free(void* ptr);
 
 #ifdef __cplusplus
