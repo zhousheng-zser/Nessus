50daf04098dd970dd8851d5937296e65d8092df8
杨汉城 <hanchengyang@glasssix.com>
2023-02-06 15:50:53 +0800

    add safety production trespass

--- parser/cpp/parser_impl.cpp
+++ parser/cpp/parser_impl.cpp
@@ -526,6 +526,9 @@ namespace glasssix::exposing::nessus
 	std::unordered_map<std::string, std::function<Json::Value(plugin_interface&, Json::Value&, param_span<std::uint8_t>&, guid&, param_span<std::uint8_t>&)>> parser_impl::impl::basic_protocol_map = [] {
 		std::unordered_map<std::string, std::function<Json::Value(plugin_interface&, Json::Value&, param_span<std::uint8_t>&, guid&, param_span<std::uint8_t>&)>> protocol_map;
 #endif
+		protocol_map["trespass.new"] = &Trespass_new_json;
+		protocol_map["trespass.delete"] = &Trespass_delete_json;
+		protocol_map["trespass.detect"] = &Trespass_detect_json;
 		protocol_map["firesmoke.new"] = &Firesmoke_new_json;
 		protocol_map["firesmoke.delete"] = &Firesmoke_delete_json;
 		protocol_map["firesmoke.detect"] = &Firesmoke_detect_json;
