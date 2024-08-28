cdb1bd57537322f98d51273e0468e550d5be2a33
jianzhang <zhangjian@glasssix.com>
2024-08-28 16:57:05 +0800

    工程化防护脸部算法

---
+++ register/cpp/VisionService/protocol_service/pump_protect_face.cpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "../service_register.hpp"
+#include <functional>
+#include <unordered_map>
+#include <iostream>
+//
+#include <pump_protect_face/detect_code.hpp>
+
+namespace glasssix::exposing::nessus::Service
+{
+	class S_Pump_Protect_Face :public Service { //Plugin
+		static unknown_object pump_mask_new(const param_hash_map<param_string, unknown_object>& params)
+		{
+			auto device = unbox<std::int32_t>(params.get_value(u8"device"));
+			auto model_type = unbox<std::int32_t>(params.get_value(u8"model_type"));
+			auto models_directory = unbox<param_string>(params.get_value(u8"models_directory"));
+
+			return vision_service_impl::impl::add_instance(MODULE_, make_exported_interface<pump_protect_face::detect_code>(models_directory, device,model_type));
+		}
+
+		static 	unknown_object pump_mask_version(const param_hash_map<param_string, unknown_object>& params)
+		{
+			auto instance = vision_service_impl::impl::get_instance<pump_protect_face::detect_code>(params);
+			return box(instance.version());
+		}
+
+		static unknown_object pump_mask_detect(const param_hash_map<param_string, unknown_object>& params)
+		{
+			constexpr std::int32_t channels = 3;
+			auto instance = vision_service_impl::impl::get_instance<pump_protect_face::detect_code>(params);
+			auto image = unbox<param_span<std::uint8_t>>(params.get_value(u8"image"));
+			auto height = unbox<std::int32_t>(params.get_value(u8"height"));
+			auto width = unbox<std::int32_t>(params.get_value(u8"width"));
+			auto params_map_abi = params.get_value(u8"params").as<exposing::param_hash_map<exposing::param_string, float>>();
+			return instance.detect(image, channels, height, width, params_map_abi);
+		}
+
+		static constexpr utf8_string_view MODULE_{ u8"pump_protect_face" };
+		static constexpr utf8_string_view NEW_{ u8"pump_protect_face.new" };
+		static constexpr utf8_string_view DELETE_{ u8"pump_protect_face.delete" };
+		static constexpr utf8_string_view VERSION_{ u8"pump_protect_face.version" };
+		static constexpr utf8_string_view detect_{ u8"pump_protect_face.detect" };
+	public:
+		virtual const void service_dump(std::unordered_map<param_string, service_function_ty>& service_map) const override {
+			service_map.try_emplace(NEW_, &pump_mask_new);
+			service_map.try_emplace(DELETE_, DELETE_FUNC);
+			service_map.try_emplace(VERSION_, &pump_mask_version);
+
+			service_map.try_emplace(detect_, &pump_mask_detect);
+		}
+	};
+
+	REGISTE_SERVICE(S_Pump_Protect_Face) //Plugin
+}
